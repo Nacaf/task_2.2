fc29c1378b43d7537bf1bcca5a2bd6822bce52cd
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:39:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//Task 2.2
+
+/*let userName = "Najafgulu";
+let userSurName = "Sharifov";
+console.log(userName[0], userSurName[0]);*/
+
+/*
+let name = "Najafgulu";
+let surName = "Sharifov"
+let fullName = name + " " + surName;
+let user = fullName.split(" ");
+console.log(user[0][0], user[1][0]);
+*/
+
+
+
+
+
+
+
+
+
+//Task 2.2 Bonus
+
+//1
+/*let fullName = prompt("Enter full name");
+let user = fullName.split(" ");
+console.log(user[1], user[0]);*/
+
+
+//2
+/*let fullName = prompt("Enter full name in upper case format");
+let user = fullName.split(" ");
+console.log(user[1], user[0]);*/
+
+
+
+
+
